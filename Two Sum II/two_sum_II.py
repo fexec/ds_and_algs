9ab4a1d9cf1c4e9db650238e95803ed890793cb6
Frank A <frank.alvarez.a14@gmail.com>
2022-02-20 08:31:49 -0500

    Create two_sum_II.py

---
+++ Two Sum II/two_sum_II.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        left = 0
+        right = int(len(nums)-1)
+        
+        while left <= right:
+
+            val = nums[left] + nums[right]
+
+            if target == val:
+                return [left + 1, right + 1]
+
+            if val < target:
+                left += 1
+
+            else:
+                right -= 1
+        return
+        
+        
