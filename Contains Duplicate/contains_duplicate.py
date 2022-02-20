5958345a935e8283dff4d20d034d0105e9f9df44
Frank A <frank.alvarez.a14@gmail.com>
2022-02-20 11:36:23 -0500

    Create contains_duplicate.py

---
+++ Contains Duplicate/contains_duplicate.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/contains-duplicate
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        map = {}
+        
+        for i in range(len(nums)):
+            
+            if nums[i] in map:
+                
+               return True
+            
+            else:
+                map[nums[i]] = i
+        
+        
+        return False
