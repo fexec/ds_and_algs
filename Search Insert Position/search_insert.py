6981a274d5d2824f779ff9b440849880fce3b922
Frank A <frank.alvarez.a14@gmail.com>
2022-02-20 11:11:25 -0500

    Update search_insert.py

--- Search Insert Position/search_insert.py
+++ Search Insert Position/search_insert.py
@@ -11,25 +11,18 @@ class Solution:
         while start <= end:
             
             mid = start + (end - start) // 2
-            
-            if  nums[mid] == target:
+           
+      
+            if nums[mid] < target:
+
+                start = mid + 1
+
+            if nums[mid] > target:
+
+                end = mid - 1
+             
+            if nums[mid] == target:
                 
                 return mid
             
-            else:
-      
-                if nums[mid] < target:
-                
-                    start = mid + 1
-                
-                else:
-
-                    end = mid - 1
-                    
-        
-        if nums[mid] < target:
-            
-            return mid + 1
-        
-        else:
-            return mid
+        return start
