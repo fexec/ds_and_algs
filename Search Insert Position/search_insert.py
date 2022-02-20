4fe7fbb02c0a6c9acf855b1075410e84585c0683
Frank A <frank.alvarez.a14@gmail.com>
2022-02-20 10:59:08 -0500

    Create search_insert.py

---
+++ Search Insert Position/search_insert.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        
+        start = 0
+        end = len(nums) - 1
+        
+        mid = 0
+        
+        while start <= end:
+            
+            mid = start + (end - start) // 2
+            
+            if  nums[mid] == target:
+                
+                return mid
+            
+            else:
+      
+                if nums[mid] < target:
+                
+                    start = mid + 1
+                
+                else:
+
+                    end = mid - 1
+                    
+        
+        if nums[mid] < target:
+            
+            return mid + 1
+        
+        else:
+            return mid
